7896a17f9e1cc63e2aac7065f2abc3c526f26bd9
seongjihyeon <sseongl6w6l@naver.com>
2024-01-12 14:30:15 +0900

    Update Baekjoon10816

--- JIHYEON/Set_and_Map_By_Hashing/Baekjoon10816.java
+++ JIHYEON/Set_and_Map_By_Hashing/Baekjoon10816.java
@@ -15,29 +15,36 @@ public class Baekjoon10816 {
         StringBuilder sb = new StringBuilder();
         HashMap<String, Integer> map = new HashMap<String, Integer>();
 
+        //Hashmap key에 값, value에 개수 저장
 		int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         while (N-- > 0) {
             String st_next = st.nextToken();
+            //해당 값을 Hashmap이 가지고 있지 않으면 key를 해당 값, value를 1로 해 추가 
             if(!map.containsKey(st_next)) {
                 map.put(st_next, 1);
             }
+            //해당 값을 Hashmap이 가지고 있으면 value에 1을 더해 변경(추가)
             else {
                 map.put(st_next, map.get(st_next) + 1);
             }
         }
 
+        //Hashmap에서 탐색
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         while (M-- > 0) {
             String st_next = st.nextToken();
+            //해당 값을 Hashmap이 가지고 있지 않으면 '0'를 출력문자열에 추가 
             if(!map.containsKey(st_next)) {
                 sb.append("0 ");
             }
+            //해당 값을 Hashmap이 가지고 있으면 value(갯수)를 출력문자열에 추가
             else {
                 sb.append(map.get(st_next)).append(" ");
             }
         }
+
         System.out.print(sb);
         br.close();
     }
