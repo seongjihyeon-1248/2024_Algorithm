3e99f0f655416ca47ea7a10a90ab9e8dbfc8521b
seongjihyeon <sseongl6w6l@naver.com>
2024-01-15 01:12:02 +0900

    update: Baekjoon_2839

--- JIHYEON/Dynimic_Programing/Baekjoon_2839.java
+++ JIHYEON/Dynimic_Programing/Baekjoon_2839.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
  
-public class Baekjoon2839 {
+public class Baekjoon_2839 {
 
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
