ab5802bd086f806861310de3be949fc5dd0873f2
chaeyeonss <rccodus@gmail.com>
2024-02-11 12:15:36 +0900

    create: baekjoon_15954.py

---
+++ CHAEYEON/baekjoon_15954.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+n, k = map(int, sys.stdin.readline().split())
+pref = list(map(int, sys.stdin.readline().split()))
+
+def variance(selection):
+    dis = 0
+    avg = sum(selection)/len(selection)
+    for d in selection:
+        dis += (d-avg)**2
+    return dis/len(selection)
+
+answer = list()
+for i in range(n-k+1):
+    for j in range(n-k-i+1):
+        tmp = pref[i:i+k+j]
+        a = variance(tmp)
+        answer.append(a)
+print(math.sqrt(min(answer)))
